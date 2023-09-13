79cd4ab0a56902310654e5f98ad92aa180458204
NamakulaAllen <143063952+NamakulaAllen@users.noreply.github.com>
2023-09-13 08:36:31 -0700

    Add files via upload

---
+++ ALLEN NAMAKULA.py
@@ -0,0 +1,42 @@
+b= 2,4,6 # even numbers less than 8
+print(sum(b))
+
+#average
+numbers=(2,4,6,8,10)
+TotalNumbers = (2+4+6+8+10)
+Average = (TotalNumbers)/5
+print(Average)
+
+
+-5+8*6
+x = -5
+y =8
+h =6
+j =y*h
+print(j+x)
+
+
+(55+9)%9
+x = 55
+y = 9
+h =(x+y)
+t = 9
+print(h%t)
+
+
+20+-3*5/8
+s = -3*(5/8)
+print(20+s)
+
+
+5+15/3*2-8%3
+y = (5+15/3)*2-8
+x = 3
+print(y%x)
+
+
+
+
+
+
+
